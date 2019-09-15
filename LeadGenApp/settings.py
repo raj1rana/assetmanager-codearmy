4da95c30390ab01fc1611a4e3535ec3edaac8303
robert.rajendra <robert.rajendra@ithands.biz>
2019-09-15 18:54:28 +0530

    adding login page

--- LeadGenApp/settings.py
+++ LeadGenApp/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'LeadGenApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'Login/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
